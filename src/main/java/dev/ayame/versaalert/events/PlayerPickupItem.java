feaeab170979623022b7f865d2f6131a0953ae07
AkariTheDev <maria7892020@gmail.com>
2023-07-20 19:22:15 +0300

    commit..

--- src/main/java/dev/ayame/versaalert/events/PlayerPickupItem.java
+++ src/main/java/dev/ayame/versaalert/events/PlayerPickupItem.java
@@ -5,31 +5,32 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import dev.ayame.versaalert.handlers.dataHandler;
 import org.bukkit.inventory.ItemStack;
-
+import org.bukkit.scheduler.BukkitRunnable;
+import dev.ayame.versaalert.VersaAlert;
 public class PlayerPickupItem implements Listener {
     @EventHandler
-    @Deprecated
     public void onPlayerPickupItem(final PlayerPickupItemEvent event) {
-       final Player player = event.getPlayer();
-        if (!canPickUpItems(player, event.getItem().getItemStack())) {
-            player.sendTitle(dataHandler.INSTANCE.getConfig("versaalert.player.inventory.full.title", true).toString(), dataHandler.INSTANCE.getConfig("versaalert.player.inventory.full.subtitle", true).toString());
-        }
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                final Player player = event.getPlayer();
+                if (!canPickUpItems(player, event.getItem().getItemStack())) {
+                    player.sendTitle(dataHandler.INSTANCE.getConfig("versaalert.player.inventory.full.title", true).toString(), dataHandler.INSTANCE.getConfig("versaalert.player.inventory.full.subtitle", true).toString());
+                }
+            }
+        }.runTaskLater(VersaAlert.INSTANCE, 1);
     }
     private boolean canPickUpItems(final Player player, final ItemStack item) {
-        boolean rtn = false;
-
         for (final ItemStack itemStack : player.getInventory().getContents()) {
             if (itemStack == null) return true;
 
             if (itemStack.getType().equals(item.getType())) {
-                if (itemStack.getAmount() == itemStack.getMaxStackSize() - item.getAmount()) {
-                    rtn = false;
-                    continue;
-                }
-                rtn = true;
+                player.sendMessage(itemStack.getAmount() + " - " + item.getMaxStackSize());
+
+                if (itemStack.getAmount() < item.getMaxStackSize()) return true;
             }
         }
-        return rtn;
+        return false;
     }
 
 }
